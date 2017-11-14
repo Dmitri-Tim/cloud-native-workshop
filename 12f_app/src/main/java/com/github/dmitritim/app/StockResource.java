bcede9b0d53cc4df0ab5611ac151204bd43f7a9d
Dmitri Timofejev <dmitri.timofejev@finestmedia.ee>
2017-11-14 11:39:19 +0200

    logging added

--- 12f_app/src/main/java/com/github/dmitritim/app/StockResource.java
+++ 12f_app/src/main/java/com/github/dmitritim/app/StockResource.java
@@ -1,6 +1,8 @@
 package com.github.dmitritim.app;
 
 import com.github.dmitritim.app.domain.model.StockItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,9 @@ import java.util.Random;
 @RestController
 @RequestMapping(value = "/stock", produces = MediaType.APPLICATION_JSON_VALUE)
 public class StockResource {
+
+    private static final Logger logger = LoggerFactory.getLogger(StockResource.class);
+
     @Autowired
     private Environment environment;
 
@@ -21,15 +26,19 @@ public class StockResource {
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public StockItem stockItem(@PathVariable("id") Long id, HttpServletResponse response) {
+        logger.info("Starting search of stock item(id={}) search", id);
 
         StockItem stockItem = stockItemRepository.findOne(id);
 
         if (stockItem == null) {
+            logger.info("Stock item(id={}) has not been found", id);
             response.setStatus(HttpStatus.NOT_FOUND.value());
             return null;
         }
 
+        logger.info("Finishing search of stock item(id={}) search", id);
         return stockItem;
+
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
